df237ac2d1f6f346d35ac4e471cfcf4f85743922
Talita Cristina Alves Lobato <05058225204@>
2025-06-10 10:33:52 -0300

    novo

--- Back/Projeto/Projeto/urls.py
+++ Back/Projeto/Projeto/urls.py
@@ -14,10 +14,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+# app/urls.py
 from django.urls import path, include
-
+from app import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('app.urls')),
+    # ProfessorGestor
+    path('professores/', views.ProfessorGestorListCreateView.as_view()),
+    path('professores/<int:Numero_indentificacao>/', views.ProfessorGestorRetriveUpdateDestroyAPIView.as_view()),
+
+    # Disciplina
+    path('disciplinas/', views.DisciplinaListCreateView.as_view()),
+    path('disciplinas/<int:pk>/', views.DisciplinaRetriverUpdateDestryAPIView.as_view()),
+    path('disciplinas/professor/', views.ListDisciplinasProfessor.as_view()),
+
+    # Sala
+    path('salas/', views.SalaListCreateAPIView.as_view()),
+    path('salas/<int:pk>/', views.SalaRetriveUpdateDestroyApiView.as_view()),
+
+    # Ambiente
+    path('ambientes/', views.AmbienteListCreateView.as_view()),
+    path('ambientes/<int:pk>/', views.AmbienteRetriverUpdateDestroyApiView.as_view()),
+
+    # Login
+    path('login/', views.LoginView.as_view()),
 ]
